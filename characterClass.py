0d8898396d3ef61d97683a6ad6b3f6f4d838ac84
Kratargon <lmdavis@caltech.edu>
2022-01-02 17:11:30 -0800

    Create characterClass.py

---
+++ characterClass.py
@@ -0,0 +1,25 @@
+# 1/2/2022
+
+class Character:
+    def __init__(self, stats, items, abilities):
+        # Expected: Dictionary (Stat -> Value)
+        self.statBlock = stats
+        # Expected: Dictionary (Location -> Item)
+        self.itemBlock = items
+        # Expected: List
+        self.abilityBlock = abilities
+
+    def levelUp(self, quant = 1):
+        self.statBlock["level"] += quant
+
+    def statChange(self, stat, quant):
+        self.statBlock[stat] += quant
+
+    def setItem(self, pos, item):
+        #TODO check if this works right with pointers
+        temp = self.itemBlock[pos]
+        self.itemBlock[pos] = item
+        return temp
+
+    def gainAbility(self, ability):
+        self.abilityBlock.append(ability)
